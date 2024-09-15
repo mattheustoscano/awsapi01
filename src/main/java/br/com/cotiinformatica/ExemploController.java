fbe850298a143d98641ece169f48324ad262516e
mattheustoscano <mattheustoscano@outlook.com>
2024-09-15 17:09:35 -0300

    creating a deploy yml file to configure a pipeline workflow

--- src/main/java/br/com/cotiinformatica/ExemploController.java
+++ src/main/java/br/com/cotiinformatica/ExemploController.java
@@ -15,10 +15,10 @@ public class ExemploController {
 	@GetMapping
 	public List<AlunosDto> get() throws Exception {
 		
+		//teste
 		var host = "jdbc:postgresql://postgresql-coti.cr2ags0gmij2.us-east-1.rds.amazonaws.com:5432/AwsApi01";
 		var user = "postgres";
 		var pass = "Coti2024!";
-		
 		
 		var connection = DriverManager.getConnection(host, user, pass);
 		var statement = connection.prepareStatement("select * from alunos order by nome");
